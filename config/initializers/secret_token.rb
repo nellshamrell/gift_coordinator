21d742cd538fcf30466b498bedb6b33d9bb3c0b6
Nell Shamrell <nshamrell@bluebox.net>
2012-03-08 14:04:34 -0800

    Generated initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiftCoordinator::Application.config.secret_token = '029cc194a88adc384097114832635b41ea9097c6f4b084daf07fcc11160f41f19292b252a4fc055f1a96e4447bb344a462bd4e6bfb7bb1f97f3b6eb3c0b07c5b'
